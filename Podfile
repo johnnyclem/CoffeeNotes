f90b965e33ecfc6d784778e56625fccf885df3ec
Cole Bratcher <colebratcher@gmail.com>
2014-05-06 14:06:00 -0700

    Ok, now the file should actually be there

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+pod 'AXRatingView', '~> 0.9.6'
+
+pod 'MZDayPicker','~> 0.0.5'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AXRatingView
+#define COCOAPODS_POD_AVAILABLE_AXRatingView
+#define COCOAPODS_VERSION_MAJOR_AXRatingView 0
+#define COCOAPODS_VERSION_MINOR_AXRatingView 9
+#define COCOAPODS_VERSION_PATCH_AXRatingView 6
+
+// MZDayPicker
+#define COCOAPODS_POD_AVAILABLE_MZDayPicker
+#define COCOAPODS_VERSION_MAJOR_MZDayPicker 0
+#define COCOAPODS_VERSION_MINOR_MZDayPicker 0
+#define COCOAPODS_VERSION_PATCH_MZDayPicker 5
+
